2e0307d1e288a06abeb99124964224395dfa7fb7
mobarak hamdy <mobarak.ashry@gmail.com>
2026-06-12 23:36:27 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import Hero from '@/components/home/Hero'
 import ProductGrid from '@/components/home/ProductGrid'
--- components/home/ProductGrid.tsx
+++ components/home/ProductGrid.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import { Product } from '@/types/product';
 import HomeProductCard from './HomeProductCard';
